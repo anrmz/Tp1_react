2a82032124b35f9e543168653cfe921bc62392d9
anrmz <anouarmezgualli17@gmail.com>
2023-10-26 00:49:55 +0100

    message

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import Calc from './Calc';
+import "./App.css";
+import Calc from "./Calc";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
-    <Calc/> 
+      <header className="App-header"></header>
+      <Calc />
     </div>
-);     
-
+  );
 }
 
 export default App;
